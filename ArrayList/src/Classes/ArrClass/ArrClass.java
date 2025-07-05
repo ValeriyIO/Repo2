4a608680fecc8210fe6cf7c1b7f3a93b697657d8
ValeriyIO <iolife.g@gmail.com>
2025-07-05 19:02:07 +0300

    New Branch + arrayList copy
    Добавлено 1 поле

---
+++ ArrayList/src/Classes/ArrClass/ArrClass.java
@@ -0,0 +1,17 @@
+package Classes.ArrClass;
+
+public class ArrClass {
+	private String arrString;
+	private String comment;
+	private String comment2;
+	private String comment3;	//ArrayList Branch
+	public ArrClass(String str, String cmt)
+	{
+		arrString = str;
+		comment = cmt;
+	}
+    private class ArrClassPriivate {
+    }
+}
+
+
