efc29b0321eff7681b782884c9ca571d0dca8ba7
ValeriyIO <iolife.g@gmail.com>
2025-07-05 18:35:29 +0300

    Добавлен конструктор в ArrClass
    Добавлен конструктор в ArrClass, выполнен коммит.

--- ArrayList/ArrayList/src/Classes/ArrClass/ArrClass.java
+++ ArrayList/ArrayList/src/Classes/ArrClass/ArrClass.java
@@ -1,7 +1,11 @@
 package Classes.ArrClass;
 
 public class ArrClass {
-
+	private String arrString;
+	public ArrClass(String str)
+	{
+		arrString = str;
+	}
     private class ArrClassPriivate {
     }
 }
