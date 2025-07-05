455d85680203cfcb4f1b50687d3219ad9daaacc2
ValeriyIO <iolife.g@gmail.com>
2025-07-05 18:46:21 +0300

    Добавлен еще 1 комментарий

--- ArrayList/ArrayList/src/Classes/ArrClass/ArrClass.java
+++ ArrayList/ArrayList/src/Classes/ArrClass/ArrClass.java
@@ -3,6 +3,7 @@ package Classes.ArrClass;
 public class ArrClass {
 	private String arrString;
 	private String comment;
+	private String comment2;
 	public ArrClass(String str, String cmt)
 	{
 		arrString = str;
